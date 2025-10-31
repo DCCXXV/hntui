0e20a2f26ca4000ec7fee1774702b3e4d4e678d2
DCCXXV <110636189+DCCXXV@users.noreply.github.com>
2025-10-31 14:15:27 +0100

    feat: comments can be seen

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
-	"strconv"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/k3a/html2text"
 	"github.com/muesli/reflow/wrap"
 )
 
@@ -62,8 +62,15 @@ type Story struct {
 	Descendants int    `json:"descendants"` // In the case of stories or polls, the total comment count.
 }
 
+type Comment struct {
+	ID   int    `json:"id"`
+	By   string `json:"by"`
+	Text string `json:"text"`
+	Time int    `json:"time"`
+}
+
 type loadedStoriesMsg struct{ loadedStories [MAX]Story }
-type loadedCommentsMsg struct{ loadedComments Story }
+type loadedCommentsMsg struct{ loadedComments []Comment }
 type errMsg struct{ err error }
 
 func fetchTopStories() tea.Msg {
@@ -117,14 +124,41 @@ func fetchTopStories() tea.Msg {
 	return loadedStoriesMsg{loadedStories: stories}
 }
 
+func fetchComments(story Story) tea.Msg {
+	var comments []Comment
+	for _, commentID := range story.Kids {
+		commentResp, err := http.Get(fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json?print=pretty", commentID))
+		if err != nil {
+			continue
+		}
+
+		commentBody, err := io.ReadAll(commentResp.Body)
+		commentResp.Body.Close()
+		if err != nil {
+			continue
+		}
+
+		var c Comment
+		if err := json.Unmarshal(commentBody, &c); err != nil {
+			continue
+		}
+		comments = append(comments, c)
+	}
+
+	return loadedCommentsMsg{loadedComments: comments}
+}
+
 type model struct {
-	stories      [MAX]Story
-	cursor       int
-	loading      bool
-	commentsView bool
-	err          error
-	width        int
-	height       int
+	stories        [MAX]Story
+	comments       []Comment
+	commentHeights []int
+	cursor         int
+	commentCursor  int
+	loading        bool
+	commentsView   bool
+	err            error
+	width          int
+	height         int
 }
 
 func initialModel() model {
@@ -149,6 +183,16 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.loading = false
 		return m, nil
 
+	case loadedCommentsMsg:
+		m.comments = msg.loadedComments
+		m.loading = false
+		m.commentCursor = 0
+		m.commentHeights = make([]int, len(m.comments))
+		for i := range m.comments {
+			m.commentHeights[i] = m.commentHeight(i)
+		}
+		return m, nil
+
 	case errMsg:
 		m.err = msg.err
 		m.loading = false
@@ -159,18 +203,32 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		case "up", "k":
-			if m.cursor > 0 {
+			if m.commentsView {
+				if m.commentCursor > 0 {
+					m.commentCursor--
+				}
+			} else if m.cursor > 0 {
 				m.cursor--
 			}
 		case "down", "j":
-			if m.cursor < len(m.stories)-1 {
+			if m.commentsView {
+				if m.commentCursor < len(m.comments)-1 {
+					m.commentCursor++
+				}
+			} else if m.cursor < len(m.stories)-1 {
 				m.cursor++
 			}
 		case "r":
 			m.loading = true
 			return m, fetchTopStories
 		case "c":
-			m.commentsView = !m.commentsView
+			if !m.commentsView {
+				m.commentsView = true
+				m.loading = true
+				return m, func() tea.Msg { return fetchComments(m.stories[m.cursor]) }
+			} else {
+				m.commentsView = false
+			}
 		case "pgup", "l":
 			page++
 			m.loading = true
@@ -186,6 +244,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
+func (m model) commentHeight(i int) int {
+	text := html2text.HTML2Text(m.comments[i].Text)
+	wrapped := wrap.String(text, m.width)
+	return lipgloss.Height(wrapped)
+}
+
 func (m model) View() string {
 	var s strings.Builder
 
@@ -193,14 +257,61 @@ func (m model) View() string {
 		return fmt.Sprintf("\n  Error: %v\n\n  Press 'r' to retry or 'q' to quit.\n", m.err)
 	}
 
-	if m.commentsView {
-		return strconv.Itoa(m.stories[m.cursor].Kids[0])
-	}
-
 	s.WriteString(titleBarStyle.Width(m.width).Render("Hacker News"))
 
 	if m.loading {
 		s.WriteString("\nLoading...\n")
+	}
+
+	if m.commentsView {
+		visibleStart := 0
+		visibleEnd := len(m.comments)
+
+		if m.height > 0 {
+			maxVisible := (m.height - 2) / 3
+			if maxVisible > 0 && maxVisible < len(m.comments) {
+				visibleStart = max(m.commentCursor-maxVisible/2, 0)
+				visibleEnd = visibleStart + maxVisible
+				if visibleEnd > len(m.comments) {
+					visibleEnd = len(m.comments)
+					visibleStart = max(visibleEnd-maxVisible, 0)
+				}
+			}
+		}
+
+		for i := visibleStart; i < visibleEnd && i < len(m.comments); i++ {
+			cursor := "  "
+			if m.commentCursor == i {
+				cursor = "> "
+			}
+			text := html2text.HTML2Text(m.comments[i].Text)
+			wrapped := wrap.String(text, m.width-2)
+
+			var comment string
+			if m.commentCursor == i {
+				comment = fmt.Sprintf("%s%s\n", cursor,
+					storyStyle.Width(m.width).
+						Background(lipgloss.Color("235")).
+						Padding(0, 1).
+						Render(wrapped))
+			} else {
+				comment = fmt.Sprintf("%s%s\n", cursor,
+					storyStyle.
+						Width(m.width).
+						Padding(0, 1).
+						Render(wrapped))
+			}
+			s.WriteString(comment)
+		}
+		/*
+			s.WriteString(
+				wrap.String(
+					"\nj/k move | c: back to stories | q: quit",
+					m.width,
+				),
+			)*/
+
+		return s.String()
 	}
 
 	visibleStart := 0
@@ -245,7 +356,7 @@ func (m model) View() string {
 
 	}
 
-	s.WriteString(wrap.String("\nj/k: move | r: refresh | enter: view in browser | h/l: move beetween pages | c: comments | q: quit", m.width))
+	s.WriteString(wrap.String("\nj/k: move | r: refresh | enter: view in browser | h/l: move between pages | c: comments | q: quit", m.width))
 
 	return s.String()
 }
